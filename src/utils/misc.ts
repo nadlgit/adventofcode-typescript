9f6cb92fb11007f625bb259060faa635e56c49db
Nadine <nadlgit@users.noreply.github.com>
2024-07-30 09:03:27 -0400

    enhanced ObjectSet class

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -14,19 +14,33 @@ export function memoize<A extends unknown[], R>(func: (...args: A) => R) {
 }
 
 export class ObjectSet<T extends object> {
-  private items: Set<string> = new Set();
+  private readonly items: Set<string>;
 
   constructor(items: T[] = []) {
-    for (const item of items) {
-      this.add(item);
-    }
+    this.items = new Set(items.map((item) => JSON.stringify(item)));
+  }
+
+  get size(): number {
+    return this.items.size;
   }
 
   has(item: T): boolean {
     return this.items.has(JSON.stringify(item));
   }
 
+  values(): T[] {
+    return [...this.items.values()].map((str) => JSON.parse(str) as T);
+  }
+
   add(item: T): void {
     this.items.add(JSON.stringify(item));
+  }
+
+  delete(item: T): void {
+    this.items.delete(JSON.stringify(item));
+  }
+
+  clear(): void {
+    this.items.clear();
   }
 }
