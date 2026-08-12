d55acf8aa2030c8cc1ff1f5e8746881d65d03ce8
Nadine <nadlgit@users.noreply.github.com>
2026-08-12 11:34:25 -0400

    chore: use eslint-plugin-only-warn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import jseslint from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
+import 'eslint-plugin-only-warn';
+
 export default defineConfig([
   {
     linterOptions: {
@@ -11,24 +13,12 @@ export default defineConfig([
   globalIgnores(['dist']),
   jseslint.configs.recommended,
   {
-    rules: {
-      'no-unused-vars': 'warn',
-      'require-await': 'warn',
-    },
-  },
-  {
     files: ['src/**/*.ts'],
     extends: [tseslint.configs.strictTypeChecked],
     languageOptions: {
       parserOptions: {
         projectService: true,
       },
-    },
-    rules: {
-      '@typescript-eslint/no-non-null-assertion': 'warn',
-      '@typescript-eslint/no-unused-expressions': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/require-await': 'warn',
     },
   },
 ]);
