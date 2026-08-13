15401d48b39f0b9fbcf746c1552ebe3af454b230
Nadine <nadlgit@users.noreply.github.com>
2026-08-13 11:48:25 -0400

    add non regression tests

---
+++ src/2022/day02/advent-day-answer.test.ts
@@ -0,0 +1,16 @@
+import { getAdventDaySolveData } from '#utils/index.js';
+import day from './index.js';
+
+const { inputFilePath, answerPart1, answerPart2 } = await getAdventDaySolveData(import.meta.url);
+
+it.runIf(inputFilePath && answerPart1)('solve part 1', async () => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const result = await day.part1.solve(inputFilePath!);
+  expect(String(result as unknown)).toBe(answerPart1);
+});
+
+it.runIf(day.part2 && inputFilePath && answerPart2)('solve part 2', async () => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const result = await day.part2!.solve(inputFilePath!);
+  expect(String(result as unknown)).toBe(answerPart2);
+});
--- src/utils/advent-day.ts
+++ src/utils/advent-day.ts
@@ -1,3 +1,8 @@
+import { createReadStream } from 'node:fs';
+import { dirname, basename, join } from 'node:path';
+import { createInterface } from 'node:readline';
+import { fileURLToPath } from 'node:url';
+
 type AdventDayPart<T> = {
   solve: (filepath: string) => T | Promise<T>;
   examples: { filename: string; expected: T }[];
@@ -7,3 +12,31 @@ export type AdventDay<T1 = number, T2 = T1> = {
   part1: AdventDayPart<T1>;
   part2?: AdventDayPart<T2>;
 };
+
+export async function getAdventDaySolveData(scriptUrl: string): Promise<{
+  inputFilePath?: string;
+  answerPart1?: string;
+  answerPart2?: string;
+}> {
+  const scriptDir = dirname(fileURLToPath(scriptUrl));
+  const scriptYear = basename(dirname(scriptDir));
+  const scriptDay = basename(scriptDir);
+  const scriptDayNum = Number.parseInt(scriptDay.replace('day', ''));
+
+  const inputFilePath = join('data', scriptYear, scriptDay, 'puzzle-input.txt');
+
+  const answersFilePath = join('data', 'puzzle-answers.md');
+  let answerPart1;
+  let answerPart2;
+  const rl = createInterface({ input: createReadStream(answersFilePath) });
+  for await (const line of rl) {
+    const [, year, day, part1, part2] = line.split('|').map((s) => s.trim());
+    if (year === scriptYear && Number.parseInt(day) === scriptDayNum) {
+      answerPart1 = part1;
+      answerPart2 = part2;
+      break;
+    }
+  }
+
+  return { inputFilePath, answerPart1, answerPart2 };
+}
